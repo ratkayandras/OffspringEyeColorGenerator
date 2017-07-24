0e44621a5997cb2d93086c45d797d20b0bdef0f5
Rátkay András <ratkay.andras@gmail.com>
2017-07-24 22:03:10 +0200

    getEyeColor fgv elkészítése + kisebb változtatások.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var nodeOffspringNumber = document.querySelector("input").value;
 nodeButton.addEventListener("click", getEyeArrayEventHandler);
 
 function getEyeArrayEventHandler(firstEye, secondEye){
-    var eyeJSON = [];
+    var eyeArray = [];
     
     var nodeTbody = document.querySelector("tbody");
     var nodeOffspringNumber = parseInt(document.querySelector("input").value);
@@ -16,21 +16,106 @@ function getEyeArrayEventHandler(firstEye, secondEye){
     var secondParentEyeSelected = document.querySelector("#secondParentEyeColor").selectedIndex;
     var secondParentEye = document.querySelectorAll(".secondParentEye")[secondParentEyeSelected].value;
 
-    eyeJSON = getEyeArray(firstParentEye, secondParentEye);
+    eyeArray = getEyeArray(firstParentEye, secondParentEye);
 
     //Itt egy for ciklussal vagy forEach fgv-el feltöltöm a táblázatok sorait.
     nodeTbody.innerHTML = "<tr>" + "<td>Ez egy cucc</td>" + "<td>Ez egy másik cucc</td>" + "</tr>"
 
-    console.log(eyeJSON);
-    console.log(nodeOffspringNumber);
-    console.log(nodeTbody);
+    getEyeColor(eyeArray);
+
+    console.log(eyeArray);
+    //console.log(nodeOffspringNumber);
+    //console.log(nodeTbody);
 }
 
 function getEyeArray(firstEye, secondEye){
-    var eyeJSON = [];
+    var eyeArray = [];
 
-    eyeJSON.push(firstEye);
-    eyeJSON.push(secondEye);
+    eyeArray.push(firstEye);
+    eyeArray.push(secondEye);
 
-    return eyeJSON;
+    return eyeArray;
+}
+
+function getEyeColor(EyePair){
+    var offspringEyeColor = "";
+    var offspringEyeColorChances = [];
+
+    if(EyePair[0] == "brown" && EyePair[1] == "brown"){
+        for(var i = 0; i < 75; i++){
+            offspringEyeColorChances.push("brown");
+        }
+        for(var i = 0; i < 19; i++){
+            offspringEyeColorChances.push("green");
+        }
+        for(var i = 0; i < 6; i++){
+            offspringEyeColorChances.push("blue");
+        }
+    }
+    if(EyePair[0] == "blue" && EyePair[1] == "blue"){
+        
+        offspringEyeColorChances.push("green");
+        
+        for(var i = 0; i < 99; i++){
+            offspringEyeColorChances.push("blue");
+        }
+    }
+    if(EyePair[0] == "green" && EyePair[1] == "green"){
+        for(var i = 0; i < 25; i++){
+            offspringEyeColorChances.push("green");
+        }
+        for(var i = 0; i < 75; i++){
+            offspringEyeColorChances.push("blue");
+        }
+    }
+    if(EyePair[0] == "brown" && EyePair[1] == "blue" || EyePair[0] == "blue" && EyePair[1] == "brown"){
+        for(var i = 0; i < 50; i++){
+            offspringEyeColorChances.push("brown");
+        }
+        for(var i = 0; i < 50; i++){
+            offspringEyeColorChances.push("green");
+        }
+    }
+    if(EyePair[0] == "brown" && EyePair[1] == "green" || EyePair[0] == "green" && EyePair[1] == "brown"){
+        for(var i = 0; i < 50; i++){
+            offspringEyeColorChances.push("brown");
+        }
+        for(var i = 0; i < 12; i++){
+            offspringEyeColorChances.push("green");
+        }
+        for(var i = 0; i < 38; i++){
+            offspringEyeColorChances.push("blue");
+        }
+    }
+    if(EyePair[0] == "blue" && EyePair[1] == "green" || EyePair[0] == "green" && EyePair[1] == "blue"){
+        for(var i = 0; i < 50; i++){
+            offspringEyeColorChances.push("green");
+        }
+        for(var i = 0; i < 50; i++){
+            offspringEyeColorChances.push("blue");
+        }
+    }
+
+    shuffle(offspringEyeColorChances);
+    //console.log(offspringEyeColorChances);
+    //console.log(offspringEyeColorChances.length);
+    offspringEyeColor = offspringEyeColorChances[(Math.random()*99).toFixed()];
+    //console.log(offspringEyeColor);
+    return offspringEyeColor;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
